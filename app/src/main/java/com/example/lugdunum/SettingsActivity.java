58749f398b61f45d73b8b3181bcc981fd76da982
Destroy-9 <david.rodhain@insa-lyon.fr>
2021-03-29 15:19:49 +0200

    add images

--- app/src/main/java/com/example/lugdunum/SettingsActivity.java
+++ app/src/main/java/com/example/lugdunum/SettingsActivity.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    private Button mRulesButton;
+    private ImageView mRulesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +18,7 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         //creation of the link between SettingsActivity and RulesActivity
-        mRulesButton = (Button) findViewById(R.id.rulesButton);
+        mRulesButton = (ImageView) findViewById(R.id.rulesButton);
         mRulesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
