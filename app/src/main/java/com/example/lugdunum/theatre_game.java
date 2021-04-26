b2b40812aba7f61bcd7e1fd80d72e05de792df41
hdesinde <hugo.desinde@outlook.fr>
2021-04-26 10:34:17 +0200

    add location in theater game

--- app/src/main/java/com/example/lugdunum/theatre_game.java
+++ app/src/main/java/com/example/lugdunum/theatre_game.java
@@ -1,17 +1,39 @@
 package com.example.lugdunum;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.AutomaticZenRule;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class theatre_game extends AppCompatActivity {
     private TextView question1;
     private ImageView img;
-    private Button found_btn;
+    public static Button found_btn;
+    private TextView mLocationView;
+
+    private String latitude;
+    private String longitude;
+    private Location mLastLocation;
+    private static final int REQUEST_LOCATION = 1;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +44,33 @@ public class theatre_game extends AppCompatActivity {
         question1 = (TextView) findViewById(R.id.question);
         img = (ImageView) findViewById(R.id.findThisPlace);
         found_btn = (Button) findViewById(R.id.yes_btn);
+        mLocationView = (TextView) findViewById(R.id.location);
+
+        found_btn.setEnabled(false);
+
+
+        // Find location
+        // Ask for permissions
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION
+        }, REQUEST_LOCATION);
+        // get location
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            onGPS();
+        } else {
+            getLocation();
+        }
+        System.out.println("Location got");
+
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pending = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        }else {
+            System.out.println("Before alertReceiver");
+            locationManager.addProximityAlert(43.706031, 7.258427, 150, -1, pending);
+        }
 
         found_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,5 +78,53 @@ public class theatre_game extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+
+    private void onGPS() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void getLocation() {
+
+        // Get the last known location
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        }else {
+            // Update location
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    mLastLocation = location;
+                    if (mLastLocation != null){
+                        latitude = String.valueOf(mLastLocation.getLatitude());
+                        longitude = String.valueOf(mLastLocation.getLongitude());
+                        mLocationView.setText("Latitude " + latitude + " et longitute " + longitude);
+                    }
+                }
+            });
+            mLastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (mLastLocation != null){
+                latitude = String.valueOf(mLastLocation.getLatitude());
+                longitude = String.valueOf(mLastLocation.getLongitude());
+                mLocationView.setText("Latitude " + latitude + " et longitute " + longitude);
+            }else {
+                Toast.makeText(this, "Unable to find location.", Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
 }
