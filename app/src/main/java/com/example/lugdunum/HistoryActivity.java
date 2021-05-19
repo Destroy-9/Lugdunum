b991e7e2ef616671196a867027ce9d0cdbf37df1
hdesinde <hugo.desinde@outlook.fr>
2021-05-19 17:56:43 +0200

    correction script
    mise en commun histoire-map

--- app/src/main/java/com/example/lugdunum/HistoryActivity.java
+++ app/src/main/java/com/example/lugdunum/HistoryActivity.java
@@ -1,8 +1,18 @@
 package com.example.lugdunum;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
@@ -12,18 +22,27 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextSwitcher;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ViewSwitcher;
 
 import com.example.lugdunum.games.CuriosityGameActivity;
 import com.example.lugdunum.games.FourviereGameActivity;
 import com.example.lugdunum.games.TheatreGameActivity;
 import com.google.android.gms.common.util.Strings;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
 
 import static android.net.wifi.WifiConfiguration.Status.strings;
 import static androidx.core.content.ContextCompat.startActivity;
 
-public class HistoryActivity extends AppCompatActivity {
+public class HistoryActivity extends FragmentActivity implements LocationListener {
 
+    // Partie histoire
     private TextSwitcher mTextHistory;
     private Button mNextBtn;
     private Button mBackBtn;
@@ -35,12 +54,30 @@ public class HistoryActivity extends AppCompatActivity {
     private int currentIndex;
     private int gameNumber;
 
+    // Partie Map
+    private LocationManager mLm;
+    public static final int PERMS_CALL_ID = 1234;
+    private GoogleMap mMap;
+    SupportMapFragment mapFragment;
+    ImageView mHelpButton;
+    //TextView mDescription;
+    Circle[] interets;
+    FragmentManager fragmentManager;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+/***************************************************************************************************
+****************************************************************************************************
+*
+**********                              Partie HISTOIRE                                   **********
+*
+****************************************************************************************************
+****************************************************************************************************/
 
         // get The references if Button and TextSwitcher
         mNextBtn = (Button) findViewById(R.id.nextButton);
@@ -65,8 +102,10 @@ public class HistoryActivity extends AppCompatActivity {
         });
 
         // Declare in and out animations and load them using AnimationUtils class
-        Animation in = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
-        Animation out = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
+        Animation in = AnimationUtils.loadAnimation(this, R.anim.slide_in_right);
+        Animation out = AnimationUtils.loadAnimation(this, R.anim.slide_out_left);
+        Animation in_back = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
+        Animation out_back = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
 
         // set the animation type to TextSwitcher
         mTextHistory.setInAnimation(in);
@@ -79,9 +118,11 @@ public class HistoryActivity extends AppCompatActivity {
         // to keep current Index of textID array
         currentIndex = -1;
 
-        //text appear on start
+        //text and image appear on start
         currentIndex++;
         mTextHistory.setCurrentText(history[currentIndex]);
+        mImage.setImageResource(R.drawable.rhino1);
+        mBackBtn.setVisibility(View.GONE);
         // ClickListener for NEXT button
         // When clicked on Button TextSwitcher will switch between labels
         // The current label will go out and next label will come in with specified animation
@@ -110,7 +151,7 @@ public class HistoryActivity extends AppCompatActivity {
                     else if (gameNumber == 4){
                         HistoryActivity.this.finish();
                     }
-                    mImage.setImageResource(R.drawable.rhino);
+                    mImage.setImageResource(R.drawable.rhino2);
                     mPoem.setImageResource(0);
                     mNextBtn.setText("Suivant");
                     scenario.incState();
@@ -119,6 +160,10 @@ public class HistoryActivity extends AppCompatActivity {
                     currentIndex = 0;
                 }
                 mTextHistory.setText(history[currentIndex]); // set Text in TextSwitcher
+
+                if (mBackBtn.getVisibility() == View.GONE){
+                    mBackBtn.setVisibility(View.VISIBLE);
+                }
             }
         });
 
@@ -126,10 +171,13 @@ public class HistoryActivity extends AppCompatActivity {
 
             public void onClick(View v) {
                 // TODO Auto-generated method stub
+                mTextHistory.setInAnimation(in_back);
+                mTextHistory.setOutAnimation(out_back);
+
                 currentIndex--;
                 // If index reaches maximum then reset it
                 if (currentIndex == -1) {
-                    mImage.setImageResource(R.drawable.rhino);
+                    mImage.setImageResource(R.drawable.rhino2);
                     mPoem.setImageResource(0);
                     mNextBtn.setText("Suivant");
                     scenario.decState();
@@ -139,12 +187,243 @@ public class HistoryActivity extends AppCompatActivity {
                     currentIndex = messageCount-1;
                 }
                 else{
-                    mImage.setImageResource(R.drawable.rhino);
+                    mImage.setImageResource(R.drawable.rhino2);
                     mPoem.setImageResource(0);
                     mNextBtn.setText("Suivant");
                 }
                 mTextHistory.setText(history[currentIndex]); // set Text in TextSwitcher
+
+                mTextHistory.setInAnimation(in);
+                mTextHistory.setOutAnimation(out);
             }
         });
+
+
+ /**************************************************************************************************
+  **************************************************************************************************
+  *
+  *********                              Partie MAP                                        *********
+  *
+  **************************************************************************************************
+  **************************************************************************************************/
+        fragmentManager = getSupportFragmentManager();
+        mapFragment = (SupportMapFragment) fragmentManager.findFragmentById(R.id.map);
+        fragmentManager.beginTransaction().hide(mapFragment).commit();
+        mImage.setVisibility(View.VISIBLE);
+        //mDescription = (TextView) findViewById(R.id.description);
+        mHelpButton = (ImageView) findViewById(R.id.HelpButton);
+        mHelpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mImage.getVisibility() == View.VISIBLE) {
+                    mImage.setVisibility(View.GONE);
+                    fragmentManager.beginTransaction().show(mapFragment).commit();
+                }
+                else {
+                    mImage.setVisibility(View.VISIBLE);
+                    fragmentManager.beginTransaction().hide(mapFragment).commit();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPermission();
+    }
+
+    private void checkPermission (){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            }, PERMS_CALL_ID);
+            return;
+        }
+
+        mLm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (mLm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            mLm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 2, this);
+        }
+        if (mLm.isProviderEnabled(LocationManager.PASSIVE_PROVIDER)) {
+            mLm.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 10000, 2, this);
+        }
+        if (mLm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            mLm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 2, this);
+        }
+        loadMap();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMS_CALL_ID) {
+            checkPermission();
+        }
+    }
+
+    private void loadMap() {
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            @SuppressWarnings("MissingPermission")
+            public void onMapReady(GoogleMap googleMap) {
+                HistoryActivity.this.mMap = googleMap;
+                googleMap.setMyLocationEnabled(true);
+                afficheCerles();
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mLm != null){
+            mLm.removeUpdates(this);
+        }
+    }
+
+
+    public void afficheCerles() {
+
+        CircleOptions cercleType1 = new CircleOptions();
+        cercleType1
+                .center(new LatLng(45.756773, 4.816251))
+                .radius(100)
+                .strokeColor(Color.RED)
+                .strokeWidth(5)
+                .fillColor(0x55FF6666);
+
+        interets = new Circle[11];
+        for (int i = 0; i < interets.length; i++) {
+            switch (i) {
+                case 0:
+                    interets[i] = mMap.addCircle(cercleType1);
+                    interets[i].setCenter(new LatLng(45.756773, 4.816251));
+                    break;
+                case 1:
+                    interets[i] = mMap.addCircle(cercleType1);
+                    interets[i].setCenter(new LatLng(45.755072, 4.821873));
+                    break;
+                case 2:
+                    interets[i] = mMap.addCircle(cercleType1);
+                    interets[i].setCenter(new LatLng(45.75617, 4.820167));
+                    break;
+                case 3:
+                    interets[i] = mMap.addCircle(cercleType1);
+                    interets[i].setCenter(new LatLng(45.756939, 4.819727));
+                    break;
+                case 4:
+                    interets[i] = mMap.addCircle(cercleType1);
+                    interets[i].setCenter(new LatLng(45.75645, 4.821691));
+                    break;
+                case 5:
+                    interets[i] = mMap.addCircle(cercleType1);
+                    interets[i].setCenter(new LatLng(45.758034, 4.821782));
+                    break;
+                case 6:
+                    interets[i] = mMap.addCircle(cercleType1);
+                    interets[i].setCenter(new LatLng(45.758713, 4.819754));
+                    break;
+                case 7:
+                    interets[i] = mMap.addCircle(cercleType1);
+                    interets[i].setCenter(new LatLng(45.762301, 4.822372));
+                    break;
+                case 8:
+                    interets[i] = mMap.addCircle(cercleType1);
+                    interets[i].setCenter(new LatLng(45.762678, 4.823016));
+                    break;
+                case 9:
+                    interets[i] = mMap.addCircle(cercleType1);
+                    interets[i].setCenter(new LatLng(45.76107, 4.824555));
+                    break;
+                case 10:
+                    interets[i] = mMap.addCircle(cercleType1);
+                    interets[i].setCenter(new LatLng(45.762111, 4.82746));
+                    break;
+                default:
+                    System.out.println("ca passe ici");
+            }
+        }
+        interets[0].setVisible(true);
+        interets[1].setVisible(false);
+        interets[2].setVisible(false);
+        interets[3].setVisible(false);
+        interets[4].setVisible(false);
+        interets[5].setVisible(false);
+        interets[6].setVisible(false);
+        interets[7].setVisible(false);
+        interets[8].setVisible(false);
+        interets[9].setVisible(false);
+        interets[10].setVisible(false);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        Toast.makeText(this, "Location: "+latitude+"/"+longitude, Toast.LENGTH_LONG).show();
+
+        if (mMap != null) {
+            LatLng here = new LatLng(latitude, longitude);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(here,15));
+            //mMap.addMarker(new MarkerOptions().position(here).title("Vous êtes ici"));
+            testPos(location);
+        }
+    }
+
+    public void testPos(@NonNull Location location){
+        int res=0;
+        Toast.makeText(this, "ca passe ici", Toast.LENGTH_LONG).show();
+        //LatLng test = new LatLng(45.756796,4.816242);
+        float results [] = new float [1];
+        while (!interets[res].isVisible() && res<10){
+            res++;
+        }
+        Location.distanceBetween(
+                interets[res].getCenter().latitude, interets[res].getCenter().longitude,
+                location.getLatitude(), location.getLongitude(),
+                results
+        );
+        if (results[0]<interets[res].getRadius()){
+            interets[res].setVisible(false);
+            interets[res+1].setVisible(true);
+            mImage.setVisibility(View.VISIBLE);
+            fragmentManager.beginTransaction().hide(mapFragment).commit();
+            setGame(res);
+
+        }
+        Toast.makeText(this, "ca passe la", Toast.LENGTH_LONG).show();
+    }
+
+    public void setGame(int numero){
+        Intent intent;
+        switch (numero){
+            case 0:
+                intent = new Intent(HistoryActivity.this, CuriosityGameActivity.class);
+                startActivity(intent);
+                break;
+            case 1:
+                intent = new Intent(HistoryActivity.this, FourviereGameActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
--- app/src/main/java/com/example/lugdunum/Scenario.java
+++ app/src/main/java/com/example/lugdunum/Scenario.java
@@ -14,8 +14,9 @@ import static androidx.core.content.ContextCompat.startActivity;
 public class Scenario {
     private int state;
     private String intro[] = {
-            "Ave *user* ! En quelle année sommes-nous ? Ah, c'est marqué là : *date*\n*year* !? Déjà !? ",
-            "Pardon, tu ne dois pas comprendre ce que je raconte. Mon nom est *name*. Je suis né en l'an 700, en plein pendant le règne de Jules César. Je crois que vous nommez cette année \"53 avant J-C\". Je n'ai jamais compris pourquoi vous considérez que Jules César a règné 50 ans avant lui-même d'ailleurs... Mais peu importe. ",
+            "Ave *user* ! Tibi gratias ago pro tui invenire me. Quid ? Vos operor non intellego quid dicam ?",
+            "Pardon, tu ne dois pas comprendre ce que je raconte. Heureusement, ta langue n'est pas si différente de la mienne. Mon nom complet est Julius Valerius Rhinus, mais utilise mon cognomen (Rhinus), c'est plus court.",
+            "Je suis né en l'an 700, en plein pendant le règne de Jules César. Je crois que vous nommez cette année \"53 avant J-C\". Je n'ai jamais compris pourquoi vous considérez que Jules César a règné 50 ans avant lui-même d'ailleurs... Mais peu importe. ",
             "En l'an 711, des intempéries diluviennent s'abattirent sur la cité de Lugdunum durant plusieurs jours, et il arriva ce dont tout le monde avait peur à l'époque : le ciel nous tomba sur la tête. Lorsque celui-ci relâcha son étreinte sur la terre, beaucoup de personnes restèrent prisonnières des nuages, dont moi. Ce fut un moment terrible. Voir tous mes amis rester à terre quand moi je m'envolais... ",
             "Mais ce n'est pas tout, car lorsque les nuages s'évaporèrent, je me suis évaporé avec eux. Cela sonna le glas de mon existence physique. Je devins un âme errante entre ciel et terre.",
             "Depuis, je me matérialise régulièrement à travers tout ce que je peux. La dernière fois que c'est arrivé, c'était lors de la tempête de 1999, j'avais pu me condenser en nuage. ",
@@ -25,7 +26,7 @@ public class Scenario {
     private String trion[] = {
             "Nous voilà devant la fontaine de Trion ! C'est marrant, elle ressemble vraiment aux fontaines qu'on trouvait à mon époque. Pourtant, elle n'a été construite qu'en 1836 pour alimenter le quartier en eau. C'est l'architecte René Dardel qui l'a construite (c'est aussi lui qui a créé la fontaine de la place Saint-Jean). ",
             "Je me rappelle de son sourire satisfait à la fin. J'avais pu capter un rayon de soleil qui m'avait projeté un instant contre la fontaine à son inauguration. J'y avais déjà fait un poc... oups. ",
-            "Il y a un autre endroit un peu plus loin que j'ai découvert sur un coup de soleil. Si tu descends encore un tout petit peu, tu vas tomber sur un parking sur ta droite. Tout au fond de ce parking (attention, il est un peu long), il y a un portail qui ressemble à ça *photo*. Rends t'y, il y a un jardin derrière si mes souvenirs sont exacts. "
+            "Il y a un autre endroit un peu plus loin que j'ai découvert sur un coup de soleil. Si tu descends encore un tout petit peu, tu vas tomber sur un parking sur ta droite. Tout au fond de ce parking (attention, il est un peu long), il y a un portail qui ressemble à ça. Tu peux t'y rendre, il y a un jardin derrière si mes souvenirs sont exacts. "
     };
     private String curiosites[] = {
             "Oh ! Tu as trouvé ! J'ai entendu dire en captant une onde radio en 2001 que ce jardin avait été offert par la ville de Montréal pour le 20 ème anniversaire des relations de coopération avec Lyon. Pour cette occasion, le sculpteur québecois Michel Goulet avait gravé 6 chaises. ",
@@ -50,7 +51,7 @@ public class Scenario {
     };
     private String theatre3[] = {
             "Ce théâtre est l'un des plus anciens du monde romain, et le plus ancien de Gaule. Il date de 15 avant J-C dans votre calendrier. À l'époque, il pouvait accueillir 10 000 spectateurs ! Même après des années de scène, j'avais toujours autant le trac avant de jouer ici. ",
-            "De l'autre côté du site archéologique, tu peux trouver *l'Odéon*, qui était utilisé pour les représentations musicales, les lectures publiques, ou les récitations poétiques. Il est beaucoup plus petit que le *Grand Théâtre*, et beaucoup plus jeune. Il n'a que 3 000 places et n'a été construit qu'à la fin du 1er siècle après J-C. ",
+            "De l'autre côté du site archéologique, tu peux trouver l'Odéon, qui était utilisé pour les représentations musicales, les lectures publiques, ou les récitations poétiques. Il est beaucoup plus petit que le Grand Théâtre, et beaucoup plus jeune. Il n'a que 3 000 places et n'a été construit qu'à la fin du 1er siècle après J-C. ",
             "Il y a aussi le prétoire du gouverneur de Lyon dans les parages, mais n'y vas pas, c'est dangereux les prétoires (c'est ce que vous appelez un palais de justice je crois maintenant). ",
             "Bon, je te laisse découvrir le site. Une fois que tu auras fait le tour, monte les marches et va en direction de la basilique de Fourvière, on se retrouve là-bas !"
     };
@@ -58,16 +59,16 @@ public class Scenario {
             "Tu as trouvé la basilique ! Elle est chouette non ? Cet endroit était le forum Vetus à mon époque (d'où le nom de Fourvière). ",
             "Si mes souvenirs son exacts, les architectes Pierre Bossan et Louis Sainte-Marie Perrin ont construit la basilique par dessus entre 1873 et 1884 pour accueillir tous les pèlerins qui venaient de plus en plus nombreux pour le culte de la Vierge Marie. ",
             "Avant sa construction, seule la tour avec la vierge en or au dessus existait. Cette magnifique statue dorée fut d'ailleurs ajoutée le 8 décembre 1852, en souvenir des échevins qui avaient imploré Marie de les délivrer de la peste deux siècles plus tôt. ",
-            "À cette occasion, tous les lyonnais ont allumé une bougie sur leur fenêtre. Ça te rappelle quelque chose ? Eh oui, la fête des lumières est née le 8 décembre 1852 ! ",
+            "À cette occasion, tous les lyonnais allumèrent une bougie sur leur fenêtre. Ça te rappelle quelque chose ? Eh oui, la fête des lumières est née le 8 décembre 1852 ! ",
             "J'ai quelques souvenirs de statues présentes dans le coin, mais je n'arrive plus à me souvenir où, Peux-tu m'aider ?"
     };
     private String fourviere2[] = {
             "Merci à toi ! C'est marrant, je remarque que la Basilique n'est toujours pas beaucoup plus avancée que la dernière fois que je suis venu. Je ne sais pas si tu as fait attention, mais l'une des statues que tu as dû retrouver avait une forme assez rectangulaire. ",
-            "Des blocs de pierre presque vierges comme celui-là, il y en a plein, mais faute de sculpteurs et de financement, ils ne sont pas terminés. Incroyable non ? ",
-            "Je te laisse contempler l'éblouissant intérieur de la Basilique Notre-Dame de Fourvière si tu le souhaites, ainsi que la magnifique vue sur la ville de Lyon qu'on peut avoir sur l'esplanade. Dis moi quand tu veux partir !"
+            "Des blocs de pierre presque vierges comme celui-là, il y en a plein, mais faute de sculpteurs et de financements, ils ne sont pas terminés. Incroyable non ? ",
+            "Je te laisse contempler l'éblouissant intérieur de la Basilique Notre-Dame de Fourvière si tu le souhaites, ainsi que la magnifique vue sur la ville de Lyon qui est disponible en allant vers l'esplanade. Dis moi quand tu veux partir !"
     };
     private String fourviere_leave[] = {
-            "Super, allons-y ! Quand tu es sur l'esplanade, il te faut descendre les escaliers qui te mèneront au *jardin du Rosaire*. La balade est plutôt agréable tu verras. ",
+            "Super, allons-y ! Quand tu es sur l'esplanade, il te faut descendre les escaliers qui te mèneront au jardin du Rosaire. La balade est plutôt agréable tu verras. ",
             "En bas du jardin, quand tu passeras le portail, il te faudra traverser la route pour prendre les escaliers. Je te laisse me dire lorsque tu arrives en bas. "
     };
     private String traboule[] = {
@@ -150,8 +151,9 @@ public class Scenario {
                 break;
             case 11: res = 4;
                 mNextBtn.setText("Finir");
+                mImage.setImageResource(R.drawable.rhino3);
                 break;
-            default: mImage.setImageResource(R.drawable.rhino);
+            default: mImage.setImageResource(R.drawable.rhino2);
                 res = 0;
                 break;
         }
